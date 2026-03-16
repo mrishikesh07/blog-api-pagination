71dde9363a984599c1526fc33c8baa0ea1234e9d
mrishikesh07 <rishikeshmasurkar7@gmail.com>
2026-03-16 12:30:18 +0530

    updated parent component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
-import './App.css'
+import usePaginationFetch from './hooks/usePaginationFetch'
+import BlogList from './components/BlogList';
 
 function App() {
-
+  const {
+    data: posts,
+    currentPage,
+    setCurrentPage,
+    totalPages,
+    isLoading} = usePaginationFetch
+    ("https://jsonplaceholder.typicode.com/posts", 5);
 
   return (
-    <>
-    
-    </>
+    <div>
+      <BlogList posts={posts}/>
+    </div>
   )
 }
 
