d3e8fecb7e48b9a62f2f99ca9f8c6b8dfe5530bc
mrishikesh07 <rishikeshmasurkar7@gmail.com>
2026-03-16 12:56:56 +0530

    Updated App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import usePaginationFetch from './hooks/usePaginationFetch'
 import BlogList from './components/BlogList';
+import PagiNation from './components/PagiNation';
+import Skeleton from './components/Skeleton';
 
 function App() {
   const {
@@ -11,9 +13,25 @@ function App() {
     isLoading} = usePaginationFetch
     ("https://jsonplaceholder.typicode.com/posts", 5);
 
+  if(isLoading){
+    return (
+      <div className='loading'>
+        <Skeleton/>
+        <Skeleton/>
+        <Skeleton/>
+        <Skeleton/>
+      </div>
+    )
+  }
   return (
-    <div>
+    <div className='container'>
+      <h1 className='title'>Blog Posts</h1>
       <BlogList posts={posts}/>
+      <PagiNation
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        totalPages={totalPages}
+      />
     </div>
   )
 }
