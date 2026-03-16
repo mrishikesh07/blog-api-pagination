dbea9e150bc732eabc9ea31cd6edf66c68c40279
mrishikesh07 <rishikeshmasurkar7@gmail.com>
2026-03-16 12:34:26 +0530

    mapped list

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const BlogList = ({posts}) => {
   return (
     <div>
-
+        {
+            posts.map((post) => (
+                <div key={post.id}>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                </div>
+            ))
+        }
     </div>
   )
 }
